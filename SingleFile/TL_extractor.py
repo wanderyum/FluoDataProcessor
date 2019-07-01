bc53bd00d6170346e08d94a914d7acd1a2ed350f
Manfredo <wangzhiyu0471@gmail.com>
2019-07-01 09:28:36 +0800

    TL_extractor updated

--- SingleFile/TL_extractor.py
+++ SingleFile/TL_extractor.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import time
-from misc import get_file_names, extract_data_from_single_file, combine_dictionaries, save_by_channel
+from misc import get_file_names#, extract_data_from_single_file, combine_dictionaries, save_by_channel
 
 sys.path.append('..')
 from dataviewer import dataViewer
@@ -12,14 +12,14 @@ class data_extractor():
         self.prefix = prefix
         self.postfix = postfix
         self.two_channel_path = two_channel_path
-        
+    '''
     def extract(self, folder, sleep_time=1):
         self.get_dat_file(folder=folder, sleep_time=sleep_time)
         D = extract_data_from_single_file(os.path.join(folder, files[0]))
         for f in files[1:]:
             D = combine_dictionaries(D, extract_data_from_single_file(os.path.join(folder,f)))
         self.target_files = save_by_channel(D, folder=self.output_folder, name=files[0][:8], prefix=self.prefix, postfix=self.postfix, index=False)
-        
+    '''
     def get_dat_file(self, folder, sleep_time):
         pcr_file = get_file_names(folder, filter='.pcr')[0]
         cmd_run_TC = 'start /b '+self.two_channel_path+' '+os.path.join(folder, pcr_file)
@@ -27,13 +27,13 @@ class data_extractor():
         time.sleep(sleep_time)
         pcr_name = pcr_file[:-4]
         cmd_copy = 'copy '+os.path.join(folder, '~'+pcr_name, pcr_name+'.dat')+' '+os.path.join(folder, pcr_name+'.dat')
-        print(cmd_copy)
+        print('copy command:\n{}'.format(cmd_copy))
         os.system(cmd_copy)
         time.sleep(sleep_time)
         cmd_kill = 'taskkill /f /t /im TwoChannel.exe'
         os.system(cmd_kill)
-        cmd_del = 'del /q '+os.path(folder, '~'+pcr_name, '*.*')+' & rd '+os.path.join(folder, '~'+pcr_name)
-        print(cmd_del)
+        cmd_del = 'del /q '+os.path.join(folder, '~'+pcr_name, '*.*')+' & rd '+os.path.join(folder, '~'+pcr_name)
+        print('delete command:\n{}'.format(cmd_del))
         os.system(cmd_del)
         
 
@@ -42,3 +42,9 @@ class data_extractor():
             viewer = dataViewer()
             viewer.load_csv(path=f, title=f)
             viewer.mainloop()
+
+if __name__ == '__main__':
+    folder = 'raw_data'
+    TwoChannelPath = 'E:\TianLongPCR2\TwoChannel.exe'
+    de = data_extractor(TwoChannelPath, output_folder=folder)
+    de.get_dat_file(folder, 1)
